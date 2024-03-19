bd4ba5de7d51c6f3d798f31b30fd411283e6757e
Sebastian Drozd <530rge@gmail.com>
2024-03-19 15:07:16 +0100

    update info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -162,9 +162,20 @@
 	Sorting is not working in the example because dummyjson.com does not support it
 </p>
 
-<h4 class="scroll-m-20 pt-6 text-xl font-semibold tracking-tight">For whom?</h4>
+
+
+
+<h4 class="scroll-m-20 pt-6 text-xl font-semibold tracking-tight">What is this?</h4>
 <p class="pb-6 leading-7">
-	The component handles data from the server. It's easy to extend with additional features, styling
+	I put into your hands just the merest component built on top of the great shadcn-svelte library
+	and built-in svelte functions like stores etc. You can change every part of the table - its
+	appearance, logic etc. This can be a great basis to build something that fits your needs
+	perfectly. Doesn't a component or basic solution suit you? Change it as you like!
+</p>
+
+<h4 class="scroll-m-20 text-xl font-semibold tracking-tight">For whom?</h4>
+<p class="pb-6 leading-7">
+	The component handles data from the server (client side soon). It's easy to extend with additional features, styling
 	shouldn't be difficult either. Use it if you already use shadcn-svelte and tailwind.
 </p>
 <h4 class="scroll-m-20 text-xl font-semibold tracking-tight">Genesis</h4>
@@ -181,7 +192,7 @@
 <p class="pb-6 leading-7">
 	I won't hide - I'm not some veteran programmer. Although the origins of my work with code date
 	back to 2017, my knowledge is scattered and unfocused only on the frontend. I write solutions for
-	my companies, so you could call me a jack of all trades and master of none.. Programming is my
+	my companies, so you could call me a jack of all trades and master of none. Programming is my
 	passion. I decided to make this 'package' public to contribute a little bit to open - source
 	projects, because I used them a lot. I have put a lot of time and heart into building this
 	component, but I am aware that it may not meet the code quality requirements of some people. I
@@ -212,15 +223,13 @@
 	<li>Fullscreen</li>
 	<li>Cell density</li>
 </ul>
-<h4 class="scroll-m-20 text-xl font-semibold tracking-tight">
-	What wont be added soon
-</h4>
+<h4 class="scroll-m-20 text-xl font-semibold tracking-tight">What wont be added soon</h4>
 <ul class="ml-6 list-disc pb-6 [&>li]:mt-2">
 	<li>Aggregation and grouping</li>
 	<li>Column pinning</li>
 </ul>
 
-<p class="self-center pb-6 italic ">
+<p class="self-center pb-6 italic">
 	If you find the project interesting feel free to give a star on <a
 		href="https://github.com/530RGE/table">github</a
 	>. Thanks! ❤️
