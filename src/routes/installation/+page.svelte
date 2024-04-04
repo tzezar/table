fb86d288db6d5762cbb4f955cf531e281f5fd93c
Sebastian Drozd <530rge@gmail.com>
2024-04-04 09:18:00 +0200

    guide update

--- src/routes/installation/+page.svelte
+++ src/routes/installation/+page.svelte
@@ -1,16 +1,14 @@
 <script>
-	import Separator from "$lib/components/ui/separator/separator.svelte";
-
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
 </script>
+
 <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Installation</h1>
-
-
-
 
 <h4 class="scroll-m-20 pt-6 text-xl font-semibold tracking-tight">shadcn-svelte</h4>
 <p>
 	I assume you already have shadcn-svelte installed, if not here is a tutorial: <a
-		class="font-medium"
+		class="font-medium italic"
 		href="https://www.shadcn-svelte.com/docs/installation"
 		>https://www.shadcn-svelte.com/docs/installation</a
 	>
@@ -19,14 +17,16 @@
 <h4 class="scroll-m-20 pt-6 text-xl font-semibold tracking-tight">Component</h4>
 <p>
 	Copy content of
-	<a class="font-medium" href="https://github.com/530RGE/table/tree/main/src/lib/components/table"
+	<a
+		class="font-medium italic"
+		href="https://github.com/530RGE/table/tree/main/src/lib/components/table"
 		>https://github.com/530RGE/table/tree/main/src/lib/components/table</a
 	>
 	into your project.
 </p>
 <p>
 	You also need <span class="font-medium">cn</span> helper function from
-	<a class="font-medium" href="https://github.com/530RGE/table/blob/main/src/lib/utils.ts"
+	<a class="font-medium italic" href="https://github.com/530RGE/table/blob/main/src/lib/utils.ts"
 		>https://github.com/530RGE/table/blob/main/src/lib/utils.ts</a
 	>.
 </p>
@@ -74,7 +74,17 @@
 	mode-watches is easy way to implement dark mode, and tanstack-query is good for data fetching
 </p>
 
+<h4 class="scroll-m-20 pt-6 text-xl font-semibold tracking-tight">04/04/2024 update</h4>
+<p class="pb-6 leading-7">
+	I rebuilt the shadcn table to use divs (easier styling, ability to change width, no weird
+	behaviour) and used them in the component. It made component cleaner and easier to read. You don't
+	have to overwrite it, just rename the folder and import in the component and you can have both the
+	official shadcn version and the one for the component built with divs.
 
+	<a href="https://github.com/tzezar/table/tree/main/src/lib/components/table/table">
+		<Button variant="link">just take a look at repo folder</Button>
+	</a>
+</p>
 
 <h4 class="scroll-m-20 pt-6 text-xl font-semibold tracking-tight">Code from examples</h4>
 <p class="pb-6 leading-7">
